3af8a294a75d1e3be71d9f4850c96e4fdc316e94
Polina Dineva <PDDineva22@codingburgas.bg>
2024-02-13 00:52:19 +0200

    Add GameEntity class structure

--- Space/Space/GameEntity.h
+++ Space/Space/GameEntity.h
@@ -1,1 +1,31 @@
 #pragma once
+#include "MathHelper.h"
+class GameEntity {
+
+public:
+	// Defines an enumeration
+	enum SPACE{local=0, world=1};
+
+	GameEntity(float x = 0.0f, float y = 0.0f);
+	~GameEntity();
+
+	void Pos(Vector2 pos); // Get the position of the entity
+	Vector2 Pos(SPACE space = world); // Allows specifying the coordinate space
+	void Rotation(float rotation); // Get the rotation of the entity
+	float Rotation(SPACE space = world); // Allows specifying the coordinate space
+
+	void Active(bool active); // Active or deactive the entity
+	bool Active();
+	void Parent(GameEntity* parent);
+	GameEntity* Parent();
+
+	virtual void Update();
+	virtual void Render();
+
+private:
+
+	Vector2 mPos;
+	float mRotation; // The rotation angle of the entity
+	bool mActive;
+	GameEntity* mParent;
+};
--- Space/Space/MathHelper.h
+++ Space/Space/MathHelper.h
@@ -1,19 +1,29 @@
 #include <math.h>
-#define DEG_TO_RAD PI/180.0f
-#define PI 3.14159265
 
+const float DEG_TO_RAD = PI / 180.0F; // Conversion factor from degrese to radians
+const float PI = 3.14159265f; // PI value
+
+// 2D Vector structure
 struct Vector2 {
+
 	float x;
 	float y;
+
+	// Constructor with default values
+	// The constructor initializes the x and y components of the vector with the default values
 	Vector2(float _x = 0.0f, float _y = 0.0f)
-		:x(_x), y(_y){}
+		:x(_x), y(_y){} // Initialized the x and y components to given values
+	
+	// Calculate the square of the magnitude of the vector
 	float MagnitudeSqr()
 	{
 		return x * x + y * y;
 	}
+	// Calculate the magnitude of the vector
 	float Magnitude() {
 		return (float)sqrt(x * x + y * y);
 	}
+	// Return a normalized version of the vector
 	Vector2 Normalized() {
 		float mag = Magnitude();
 		return Vector2(x / mag, y / mag);
@@ -27,8 +37,12 @@ inline Vector2 operator+(const Vector2& Ihs, const Vector2& rhs)
 inline Vector2 operator-(const Vector2& Ihs, const Vector2& rhs) {
 	return Vector2(Ihs.x - rhs.x, Ihs.y - rhs.y);
 }
+
+// Function to rotate a vector by a specified angle in degrees
 Vector2 RotateVector(Vector2& vec, float angle)
 {
-	float radAngle = (float)(angle * DEG_TO_RAD);
-	return Vector2((float)(vec.x * cos(radAngle) - vec.y * sin(radAngle)), (float)(vec.x * sin(radAngle) + vec.y * cos(radAngle)));
+	float radAngle = (float)(angle * DEG_TO_RAD); // Convert angle to radians
+	// Returns a new Vector2 representing the input vector rotated by the specified angle
+	// The rotation is achieved by applying the equations for rotating a vector in 2D space using cosine and sine functions
+	return Vector2((float)(vec.x * cos(radAngle) - vec.y * sin(radAngle)), (float)(vec.x * sin(radAngle) + vec.y * cos(radAngle))); // Rotate the vector using trigonometric function
 }
