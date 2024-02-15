d235ba3764c224d512d9310d902b64d244082bab
Polina Dineva <PDDineva22@codingburgas.bg>
2024-02-15 02:18:06 +0200

    Add a destructor and functions for proper memory management

--- Space/Space/AssetManager.cpp
+++ Space/Space/AssetManager.cpp
@@ -18,3 +18,29 @@ AssetManager::AssetManager()
 {
 
 }
+AssetManager::~AssetManager()
+{
+	// Iterates through the assetTextures map, which stores texture assets as key-value pairs
+	for (std::pair<std::string, SDL_Texture*> texture : assetTextures)
+	{
+		if (texture.second != nullptr) // Checks if the texture pointer is not nullptr, then destroys the SDL_Texture
+		{
+			SDL_DestroyTexture(texture.second);
+		}
+	}
+	assetTextures.clear();  // Clears the assetTextures map
+}
+
+// Loading and returning a texture based on filename
+SDL_Texture* AssetManager::GetTexture(std::string filename)
+{
+	// Constructs the full path by appending the filename to the base path obtained from SDL_GetBasePath
+	std::string fullPath = SDL_GetBasePath();
+	fullPath.append("Assets/" + filename);
+
+	// Load the texture using and store it in the assetTextures map
+	if (assetTextures[fullPath] == nullptr)
+		assetTextures[fullPath] = GameGraphics::Instance()->LoadTexture(fullPath);
+
+	return assetTextures[fullPath]; // Return the texture associated with the fullPath key in the assetTextures map
+}
