24bf41a0bd3ae4bc8333496c3dca8952921d044c
DDStilova22 <DDStoilova22@codingburgas.bg>
2024-02-17 23:07:55 +0200

    Add start menu button class

--- Space/Space/Button.h
+++ Space/Space/Button.h
@@ -1,1 +1,19 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
 
+class Button {
+public:
+	SDL_Rect buttonRect;
+
+	Button(int xStart, int yStart, int xEnd, int yEnd);
+
+	bool isMouseOverButton(int mouseX, int mouseY);
+
+	void handleButtonClick();
+
+	void exitGame();
+
+	void stepsGame();
+};
